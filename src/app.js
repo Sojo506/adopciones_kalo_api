72810035deea3465900e2b8ee987d6e9c05913e6
Sojo506 <fsojodev@gmail.com>
2026-03-14 21:48:56 -0600

    delete files and modify init config in app.js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
+
 const app = express();
 
 app.use(cors({
@@ -13,10 +14,20 @@ app.use(helmet());
 app.use(morgan("dev"));
 app.use(express.json());
 
-//app.use("/api/auth", require("./routes"));
+app.use("/api", require("./routes"));
 
 app.get("/", (req, res) => {
     res.send("This is the API for Kalo Adoptions");
+});
+
+app.use((err, req, res, next) => {
+    console.error(err);
+
+    res.status(500).json({
+        ok: false,
+        message: "Error interno del servidor",
+        error: err.message
+    });
 });
 
 module.exports = app;
