77d5856229267ec6767c38d8e2155bbb49986326
Sojo506 <fsojodev@gmail.com>
2026-03-14 21:43:52 -0600

    db config

--- src/config/db.js
+++ src/config/db.js
@@ -3,22 +3,30 @@ require('dotenv').config();
 
 async function getConnection() {
 
-    try {
-        const connection = await oracledb.getConnection({
-            user: process.env.DB_USER,
-            password: process.env.DB_PASSWORD,
-            connectString: process.env.DB_CONNECT_STRING,
-            configDir: process.env.WALLET_DIR,
-            walletLocation: process.env.WALLET_DIR,
-            walletPassword: process.env.DB_PASSWORD,
-            sslServerDnMatch: true
-        });
-        console.log('Connected to Oracle Database');
-        return connection;
-    } catch (err) {
-        console.error(err);
-        throw err;
-    }
+  try {
+
+    const connection = await oracledb.getConnection({
+
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      connectString: process.env.DB_CONNECT_STRING,
+      configDir: process.env.WALLET_DIR,
+      walletLocation: process.env.WALLET_DIR,
+      walletPassword: process.env.DB_WALLET_PASSWORD,
+      sslServerDnMatch: true
+
+    });
+
+    return connection;
+
+  } catch (err) {
+
+    console.error(err);
+
+    throw err;
+
+  }
+
 }
 
 module.exports = { getConnection };
