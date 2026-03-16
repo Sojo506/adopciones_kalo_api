88c95902fc42b5cf6f7fda9d69aec1597b29d7ef
Sojo506 <fsojodev@gmail.com>
2026-03-15 23:40:58 -0600

    Register location routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const router = express.Router();
 
-router.use('/users', require('./userRoutes'));
+router.use('/auth', require('./userRoutes'));
+router.use('/locations', require('./locationRoutes'));
 
 module.exports = router;
