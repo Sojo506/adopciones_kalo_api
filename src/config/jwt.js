1efb03e992e5828196ca4967a6d8386b7b58b9d0
Sojo506 <fsojodev@gmail.com>
2026-03-15 23:44:20 -0600

    add functions to gerate token, refresh, and verify them

--- src/config/jwt.js
+++ src/config/jwt.js
@@ -1,6 +1,32 @@
-module.exports = {
+const jwt = require('jsonwebtoken');
+
+const config = {
     accessSecret: process.env.JWT_ACCESS_SECRET,
     refreshSecret: process.env.JWT_REFRESH_SECRET,
     accessExpires: "15m",
     refreshExpires: "7d"
+};
+
+function generateAccessToken(payload) {
+    return jwt.sign(payload, config.accessSecret, { expiresIn: config.accessExpires });
+}
+
+function generateRefreshToken(payload) {
+    return jwt.sign(payload, config.refreshSecret, { expiresIn: config.refreshExpires });
+}
+
+function verifyAccessToken(token) {
+    return jwt.verify(token, config.accessSecret);
+}
+
+function verifyRefreshToken(token) {
+    return jwt.verify(token, config.refreshSecret);
+}
+
+module.exports = {
+    ...config,
+    generateAccessToken,
+    generateRefreshToken,
+    verifyAccessToken,
+    verifyRefreshToken
 };
