4d27943612ffb02ddab06a88602652c12194f39c
Sojo506 <fsojodev@gmail.com>
2026-03-15 23:40:58 -0600

    Add auth verification routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const userController = require('../controllers/userController');
+const { authenticateToken } = require('../middlewares/auth');
 
 const router = express.Router();
 
 router.get('/', userController.getUsers);
+router.get('/me', authenticateToken, userController.getMe);
+router.post('/register', userController.signUpValidation, userController.signUp);
+router.post('/login', userController.signInValidation, userController.signIn);
+router.post('/verify-email', userController.verifyEmailValidation, userController.verifyEmail);
+router.post('/resend-verification-email', userController.resendVerificationEmailValidation, userController.resendVerificationEmail);
 
 module.exports = router;
