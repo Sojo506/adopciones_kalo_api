062bd7efd50dca113d8f5b2bfb054e192ce5489b
Sojo506 <fsojodev@gmail.com>
2026-03-17 23:23:05 -0600

    feat: add category catalog repository lookup

--- src/repositories/catalogRepository.js
+++ src/repositories/catalogRepository.js
@@ -138,9 +138,43 @@ async function findOtpTypeById(idTipoOtp) {
     }
 }
 
+async function findCategories() {
+    let connection;
+
+    try {
+        connection = await getConnection();
+        const sql = `
+      BEGIN
+        :${OUT_CURSOR_BIND_NAME} := KALO.FIDE_KALO_PKG.FIDE_OBTENER_CATEGORIAS_FN();
+      END;
+    `;
+
+        const result = await connection.execute(
+            sql,
+            {
+                [OUT_CURSOR_BIND_NAME]: { dir: oracledb.BIND_OUT, type: oracledb.CURSOR }
+            },
+            { outFormat: oracledb.OUT_FORMAT_OBJECT }
+        );
+
+        const resultSet = result.outBinds[OUT_CURSOR_BIND_NAME];
+
+        try {
+            return await fetchRowsFromCursor(resultSet);
+        } finally {
+            await resultSet.close();
+        }
+    } finally {
+        if (connection) {
+            await connection.close();
+        }
+    }
+}
+
 module.exports = {
     findUserTypes,
     findStates,
     findOtpTypes,
-    findOtpTypeById
+    findOtpTypeById,
+    findCategories
 };
