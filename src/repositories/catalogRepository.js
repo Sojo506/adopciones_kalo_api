dfeadc861b3b7a4acfe62f2ea819067aa5833982
Sojo506 <fsojodev@gmail.com>
2026-03-18 10:30:28 -0600

    feat: add request type catalog repository lookup

--- src/repositories/catalogRepository.js
+++ src/repositories/catalogRepository.js
@@ -303,6 +303,39 @@ async function findSexes() {
     }
 }
 
+async function findRequestTypes() {
+    let connection;
+
+    try {
+        connection = await getConnection();
+        const sql = `
+      BEGIN
+        :${OUT_CURSOR_BIND_NAME} := KALO.FIDE_KALO_PKG.FIDE_OBTENER_TIPOS_SOLICITUD_FN();
+      END;
+    `;
+
+        const result = await connection.execute(
+            sql,
+            {
+                [OUT_CURSOR_BIND_NAME]: { dir: oracledb.BIND_OUT, type: oracledb.CURSOR }
+            },
+            { outFormat: oracledb.OUT_FORMAT_OBJECT }
+        );
+
+        const resultSet = result.outBinds[OUT_CURSOR_BIND_NAME];
+
+        try {
+            return await fetchRowsFromCursor(resultSet);
+        } finally {
+            await resultSet.close();
+        }
+    } finally {
+        if (connection) {
+            await connection.close();
+        }
+    }
+}
+
 module.exports = {
     findUserTypes,
     findStates,
@@ -312,5 +345,6 @@ module.exports = {
     findBrands,
     findCurrencies,
     findBreeds,
-    findSexes
+    findSexes,
+    findRequestTypes
 };
