3c184925c449cb2227c94ed729a865a4308a2342
Sojo506 <fsojodev@gmail.com>
2026-03-18 16:55:50 -0600

    feat: wire inventory Oracle package support

--- src/routes/index.js
+++ src/routes/index.js
@@ -16,6 +16,8 @@ router.use('/categories', require('./categoryRoutes'));
 router.use('/brands', require('./brandRoutes'));
 router.use('/products', require('./productRoutes'));
 router.use('/product-images', require('./productImageRoutes'));
+router.use('/inventories', require('./inventoryRoutes'));
+router.use('/inventory-movements', require('./inventoryMovementRoutes'));
 router.use('/currencies', require('./currencyRoutes'));
 router.use('/breeds', require('./breedRoutes'));
 router.use('/sexes', require('./sexRoutes'));
