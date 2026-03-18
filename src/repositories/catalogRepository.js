e19f66e2597d7548f729f571135edbf75c7086df
Sojo506 <fsojodev@gmail.com>
2026-03-18 15:29:59 -0600

    feat: add product catalog repository lookup

--- src/repositories/catalogRepository.js
+++ src/repositories/catalogRepository.js
@@ -204,6 +204,39 @@ async function findBrands() {
     }
 }
 
+async function findProducts() {
+    let connection;
+
+    try {
+        connection = await getConnection();
+        const sql = `
+      BEGIN
+        :${OUT_CURSOR_BIND_NAME} := KALO.FIDE_KALO_PKG.FIDE_OBTENER_PRODUCTOS_FN();
+      END;
+    `;
+
+        const result = await connection.execute(
+            sql,
+            {
+                [OUT_CURSOR_BIND_NAME]: { dir: oracledb.BIND_OUT, type: oracledb.CURSOR }
+            },
+            { outFormat: oracledb.OUT_FORMAT_OBJECT }
+        );
+
+        const resultSet = result.outBinds[OUT_CURSOR_BIND_NAME];
+
+        try {
+            return await fetchRowsFromCursor(resultSet);
+        } finally {
+            await resultSet.close();
+        }
+    } finally {
+        if (connection) {
+            await connection.close();
+        }
+    }
+}
+
 async function findCurrencies() {
     let connection;
 
@@ -475,6 +508,7 @@ module.exports = {
     findOtpTypeById,
     findCategories,
     findBrands,
+    findProducts,
     findCurrencies,
     findBreeds,
     findSexes,
