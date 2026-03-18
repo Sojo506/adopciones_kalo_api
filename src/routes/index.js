3625a410c7a30dc1194495827f96a7d5cf3e2b56
Sojo506 <fsojodev@gmail.com>
2026-03-18 11:13:06 -0600

    feat: register event type admin route

--- src/routes/index.js
+++ src/routes/index.js
@@ -20,6 +20,7 @@ router.use('/sexes', require('./sexRoutes'));
 router.use('/request-types', require('./requestTypeRoutes'));
 router.use('/response-types', require('./responseTypeRoutes'));
 router.use('/tracking-types', require('./trackingTypeRoutes'));
+router.use('/event-types', require('./eventTypeRoutes'));
 router.use('/user-types', require('./userTypeRoutes'));
 router.use('/emails', require('./emailRoutes'));
 router.use('/phones', require('./phoneRoutes'));
