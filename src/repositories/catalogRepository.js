a1a7cc0fdee7d062608efb75b83c0792bfe61578
Sojo506 <fsojodev@gmail.com>
2026-03-18 11:13:09 -0600

    feat: add event type catalog repository lookup

--- src/repositories/catalogRepository.js
+++ src/repositories/catalogRepository.js
@@ -402,6 +402,39 @@ async function findTrackingTypes() {
     }
 }
 
+async function findEventTypes() {
+    let connection;
+
+    try {
+        connection = await getConnection();
+        const sql = `
+      BEGIN
+        :${OUT_CURSOR_BIND_NAME} := KALO.FIDE_KALO_PKG.FIDE_OBTENER_TIPOS_EVENTO_FN();
+      END;
+    `;
+
+        const result = await connection.execute(
+            sql,
+            {
+                [OUT_CURSOR_BIND_NAME]: { dir: oracledb.BIND_OUT, type: oracledb.CURSOR }
+            },
+            { outFormat: oracledb.OUT_FORMAT_OBJECT }
+        );
+
+        const resultSet = result.outBinds[OUT_CURSOR_BIND_NAME];
+
+        try {
+            return await fetchRowsFromCursor(resultSet);
+        } finally {
+            await resultSet.close();
+        }
+    } finally {
+        if (connection) {
+            await connection.close();
+        }
+    }
+}
+
 module.exports = {
     findUserTypes,
     findStates,
@@ -414,5 +447,6 @@ module.exports = {
     findSexes,
     findRequestTypes,
     findResponseTypes,
-    findTrackingTypes
+    findTrackingTypes,
+    findEventTypes
 };
