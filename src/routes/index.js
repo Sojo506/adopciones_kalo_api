b7318133a6f4631c19a6e078c9a74b7e32cac04e
Sojo506 <fsojodev@gmail.com>
2026-03-17 23:40:44 -0600

    feat: register brand admin route

--- src/routes/index.js
+++ src/routes/index.js
@@ -13,6 +13,7 @@ router.use('/addresses', require('./addressRoutes'));
 router.use('/states', require('./stateRoutes'));
 router.use('/otp-types', require('./otpTypeRoutes'));
 router.use('/categories', require('./categoryRoutes'));
+router.use('/brands', require('./brandRoutes'));
 router.use('/user-types', require('./userTypeRoutes'));
 router.use('/emails', require('./emailRoutes'));
 router.use('/phones', require('./phoneRoutes'));
