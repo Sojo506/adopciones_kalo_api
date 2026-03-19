67175591cd9d533c54d6028f389505f9329303e0
Sojo506 <fsojodev@gmail.com>
2026-03-18 18:44:38 -0600

    feat: add movement type catalog access

--- src/routes/catalogRoutes.js
+++ src/routes/catalogRoutes.js
@@ -8,6 +8,7 @@ router.get('/states', catalogController.getStates);
 router.get('/otp-types', catalogController.getOtpTypes);
 router.get('/categories', catalogController.getCategories);
 router.get('/brands', catalogController.getBrands);
+router.get('/movement-types', catalogController.getMovementTypes);
 router.get('/products', catalogController.getProducts);
 router.get('/currencies', catalogController.getCurrencies);
 router.get('/breeds', catalogController.getBreeds);
