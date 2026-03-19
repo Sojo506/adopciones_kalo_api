a99cd8c93417255836950248db1c357fbfcdfe62
Sojo506 <fsojodev@gmail.com>
2026-03-18 18:44:45 -0600

    feat: add movement type admin backend

--- src/routes/index.js
+++ src/routes/index.js
@@ -14,6 +14,7 @@ router.use('/states', require('./stateRoutes'));
 router.use('/otp-types', require('./otpTypeRoutes'));
 router.use('/categories', require('./categoryRoutes'));
 router.use('/brands', require('./brandRoutes'));
+router.use('/movement-types', require('./movementTypeRoutes'));
 router.use('/products', require('./productRoutes'));
 router.use('/product-images', require('./productImageRoutes'));
 router.use('/inventories', require('./inventoryRoutes'));
